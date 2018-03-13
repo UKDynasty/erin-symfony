b51e72bf0c20997d891df9d71f92e5d1b5b9ca16
Dan Abrey <danabrey@gmail.com>
2018-03-13 23:36:35 +0000

    migration is safe on mysql and sqlite

--- src/Migrations/Version20180313233021.php
+++ src/Migrations/Version20180313233021.php
@@ -12,8 +12,6 @@ class Version20180313233021 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('CREATE TABLE franchise (id INTEGER NOT NULL, owner_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, mfl_franchise_id VARCHAR(255) NOT NULL, espn_franchise_id VARCHAR(255) NOT NULL, identifiers CLOB NOT NULL --(DC2Type:simple_array)
         , PRIMARY KEY(id))');
@@ -25,8 +23,6 @@ class Version20180313233021 extends AbstractMigration
 
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('DROP TABLE franchise');
         $this->addSql('DROP TABLE owner');
