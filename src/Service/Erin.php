4d3e69f24fcb0d0aa7c02d777f24e7302da1fa55
Dan Abrey <danabrey@gmail.com>
2018-04-25 12:22:18 +0100

    Case insensitive for 'who owns' and code cleanup

--- src/Service/Erin.php
+++ src/Service/Erin.php
@@ -9,21 +9,19 @@ use App\GroupMe\DirectMessage;
 use App\GroupMe\GroupMessage;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class Erin
 {
     private const IDENTIFIABLE_MESSAGES = [
-        "/hello/" => "hello",
-        "/hi/" => "hi",
-        "/\bround\b/i" => "picksRound",
-        "/\bpick\b/i" => "pick",
-        "/\bpicks\b/i" => "picks",
-        "/\bthanks\b/i" => "thanks",
-        "/\broster\b/i" => "roster",
-        "/\bowns\b/i" => "whoOwns",
-        "/\bbait\b/i" => "tradeBait",
+        '/hello/' => 'hello',
+        '/hi/' => 'hi',
+        "/\bround\b/i" => 'picksRound',
+        "/\bpick\b/i" => 'pick',
+        "/\bpicks\b/i" => 'picks',
+        "/\bthanks\b/i" => 'thanks',
+        "/\broster\b/i" => 'roster',
+        "/\bowns\b/i" => 'whoOwns',
+        "/\bbait\b/i" => 'tradeBait',
     ];
     /**
      * @var GroupMe
@@ -63,7 +61,7 @@ class Erin
     public function getOwnerFromMessageSenderId($senderId)
     {
         return $this->em->getRepository(Owner::class)->findOneBy([
-            "groupMeUserId" => $senderId,
+            'groupMeUserId' => $senderId,
         ]);
     }
 
@@ -71,48 +69,53 @@ class Erin
     {
         // Mock a $groupMeMessage
         $mockGroupMeMessage = [
-            "sender_id" => "sandbox",
-            "text" => $message,
+            'sender_id' => 'sandbox',
+            'text' => $message,
         ];
         return $this->parseMessage($mockGroupMeMessage);
     }
 
+    /**
+     * @param $groupMeMessage
+     * @return bool
+     * @throws \Exception
+     */
     public function receiveDirectMessage($groupMeMessage)
     {
 
-        if ($groupMeMessage["sender_id"] === "47997687" || $groupMeMessage["sender_id"] === "585743") {
+        if ($groupMeMessage['sender_id'] === '47997687' || $groupMeMessage['sender_id'] === '585743') {
             return false;
         }
 
         $replyText = $this->parseMessage($groupMeMessage);
 
         $reply = new DirectMessage();
-        $reply->setRecipientId($groupMeMessage["sender_id"]);
-        if (($owner = $this->getOwnerFromMessageSenderId($groupMeMessage["sender_id"])) && (0 === rand(0,5))) {
-            $replyText = "Hey " . $owner->getName() . ". " . $replyText;
+        $reply->setRecipientId($groupMeMessage['sender_id']);
+        if ((0 === random_int(0,5)) && ($owner = $this->getOwnerFromMessageSenderId($groupMeMessage['sender_id']))) {
+            $replyText = 'Hey ' . $owner->getName() . '. ' . $replyText;
         }
         $reply->setText($replyText);
         $this->groupMe->sendDirectMessage($reply);
         return true;
     }
 
-    public function receiveGroupMessage($groupMeMessage)
+    public function receiveGroupMessage($groupMeMessage): ?bool
     {
 
-        if ($groupMeMessage["sender_id"] === "47997687" || $groupMeMessage["sender_id"] === "585743") {
+        if ($groupMeMessage['sender_id'] === '47997687' || $groupMeMessage['sender_id'] === '585743') {
             return false;
         }
 
-        if (!preg_match("/^erin/i", $groupMeMessage["text"]) && !preg_match("/erin\??$/i", $groupMeMessage["text"])) {
+        if (!0 === stripos($groupMeMessage['text'], 'erin') && !preg_match("/erin\??$/i", $groupMeMessage['text'])) {
             return false;
         }
 
         $replyText = $this->parseMessage($groupMeMessage);
 
         $reply = new DirectMessage();
-        $reply->setRecipientId($groupMeMessage["sender_id"]);
-        if (($owner = $this->getOwnerFromMessageSenderId($groupMeMessage["sender_id"])) && (0 === rand(0,5))) {
-            $replyText = "Hey " . $owner->getName() . ". " . $replyText;
+        $reply->setRecipientId($groupMeMessage['sender_id']);
+        if (($owner = $this->getOwnerFromMessageSenderId($groupMeMessage['sender_id'])) && (0 === rand(0,5))) {
+            $replyText = 'Hey ' . $owner->getName() . '. ' . $replyText;
         }
         $reply = new GroupMessage();
         $reply->setText($replyText);
@@ -122,7 +125,7 @@ class Erin
     private function parseMessage(array $groupMeMessage)
     {
         foreach(static::IDENTIFIABLE_MESSAGES as $pattern => $callback) {
-            if (preg_match($pattern, $groupMeMessage["text"])) {
+            if (preg_match($pattern, $groupMeMessage['text'])) {
                 return $this->$callback($groupMeMessage);
             }
         }
@@ -131,12 +134,12 @@ class Erin
 
     private function hello()
     {
-        return "Oh hey there";
+        return 'Oh hey there';
     }
 
     private function hi()
     {
-        return "Oh hi there";
+        return 'Oh hi there';
     }
 
 //    private function pick($message)
@@ -161,10 +164,14 @@ class Erin
 //        }
 //    }
 
+    /**
+     * @param $message
+     * @return string
+     */
     private function pick($message)
     {
 
-        preg_match("/(\d+)\.(\d+)/", $message["text"], $matches);
+        preg_match("/(\d+)\.(\d+)/", $message['text'], $matches);
 
         if (!$matches) {
             return "I can't work out which pick you mean - make sure you're formatting it as round.number - e.g. pick 2.02";
@@ -174,16 +181,16 @@ class Erin
         $number = (int)$matches[2];
 
         $pick = $this->em->getRepository(DraftPick::class)->findOneBy([
-            "draft" => $this->draftManager->getCurrentDraft(),
-            "round" => $round,
-            "number" => $number,
+            'draft' => $this->draftManager->getCurrentDraft(),
+            'round' => $round,
+            'number' => $number,
         ]);
 
         if ($pick) {
-            return sprintf("The %s currently own pick %s.%s", $pick->getOwner()->getName(), $round, str_pad($pick->getNumber(), 2, "0", STR_PAD_LEFT));
-        } else {
-            return "That pick doesn't exist in the database. Ask the commish what's going on.";
+            return sprintf('The %s currently own pick %s.%s', $pick->getOwner()->getName(), $round, str_pad($pick->getNumber(), 2, '0', STR_PAD_LEFT));
         }
+
+        return "That pick doesn't exist in the database. Ask the commish what's going on.";
     }
 
 //    private function picks($message)
@@ -202,7 +209,7 @@ class Erin
     private function picks($message)
     {
         // Identify the franchise that's mentioned in the message
-        $franchise = $this->messageDataExtractor->extractFranchise($message["text"]);
+        $franchise = $this->messageDataExtractor->extractFranchise($message['text']);
         // If the franchise can't be identified, return a message saying as much
         if (!$franchise) {
             return "Sorry, I don't know which franchise you're asking about. I could guess, but that would be less than useful.";
@@ -212,24 +219,24 @@ class Erin
         /** @var Picks[] $picks */
         $picks = $this->em->getRepository(DraftPick::class)->getUnusedPicksForFranchise($franchise);
 
-        return "Picks for the " . $franchise->getName() . ":\n\n" . implode("\n", $picks);
+        return 'Picks for the ' . $franchise->getName() . ":\n\n" . implode("\n", $picks);
     }
 
     private function picksRound($message)
     {
         $draft = $this->draftManager->getCurrentDraft();
 
-        preg_match('/\d+/',$message["text"],$matches);
+        preg_match('/\d+/',$message['text'],$matches);
         if (!$matches) {
-            return "Which round do you want to see the draft order for? Say something like \"Show me the picks for round 3\"";
+            return 'Which round do you want to see the draft order for? Say something like "Show me the picks for round 3"';
         }
         $round = $matches[0];
 
         /** @var Picks[] $picks */
         $picks = $this->em->getRepository(DraftPick::class)->findBy([
-            "draft" => $draft,
-            "round" => $round,
-        ], ["number" => "ASC"]);
+            'draft' => $draft,
+            'round' => $round,
+        ], ['number' => 'ASC']);
 
         return sprintf("%s Draft Round %s:\n\n%s", $draft->getYear(), $round, $this->helpers->roundOfPicksToList($picks));
     }
@@ -237,7 +244,7 @@ class Erin
     private function roster($message)
     {
         // Identify the franchise that's mentioned in the message
-        $franchise = $this->messageDataExtractor->extractFranchise($message["text"]);
+        $franchise = $this->messageDataExtractor->extractFranchise($message['text']);
         // If the franchise can't be identified, return a message saying as much
         if (!$franchise) {
             return "Sorry, I don't know which franchise you're asking about. I could guess, but that would be less than useful.";
@@ -254,7 +261,7 @@ class Erin
     private function tradeBait($message)
     {
         // Identify the franchise that's mentioned in the message
-        $franchise = $this->messageDataExtractor->extractFranchise($message["text"]);
+        $franchise = $this->messageDataExtractor->extractFranchise($message['text']);
         // If the franchise can't be identified, return a message saying as much
         if (!$franchise) {
             return "Sorry, I don't know which franchise you're asking about. I could guess, but that would be less than useful.";
@@ -274,27 +281,27 @@ class Erin
 
     private function thanks($message)
     {
-        $messages = ["No problem!", "Happy to help.", "Don't mention it!"];
+        $messages = ['No problem!', 'Happy to help.', "Don't mention it!"];
         return $messages[array_rand($messages)];
     }
 
     private function whoOwns($message)
     {
-        preg_match("/\bwho owns\b(.*)/", rtrim(trim($message["text"]), "?"), $matches);
+        preg_match("/\bwho owns\b(.*)/i", rtrim(trim($message['text']), '?'), $matches);
         if ($matches) {
             $playerName = trim($matches[1]);
-            $explodedName = explode(" ", $playerName);
+            $explodedName = explode(' ', $playerName);
             /** @var ArrayCollection $results */
             $results = $this->em->getRepository(Player::class)->findBy([
-                "firstName" => $explodedName[0],
-                "lastName" => $explodedName[1],
+                'firstName' => $explodedName[0],
+                'lastName' => $explodedName[1],
             ]);
             if (0 === count($results)) {
                 return "I can't find a player by that name, sorry.";
             }
             if (1 === count($results)) {
                 if ($results[0]->getFranchise() && $results[0]->isListedAsTradeBait()) {
-                    return sprintf("%s is owned by the %s, and is currently listed as trade bait.", $results[0]->getName(), $results[0]->getFranchise()->getName());
+                    return sprintf('%s is owned by the %s, and is currently listed as trade bait.', $results[0]->getName(), $results[0]->getFranchise()->getName());
                 }
                 /** @var Franchise $franchise */
                 $franchise = $results[0]->getFranchise();
@@ -302,9 +309,9 @@ class Erin
                     if ($franchise->getOwner() === $this->getOwnerFromMessageSenderId($message['sender_id'])) {
                         return sprintf("%s is owned by the %s, but you already know that, don't you?", $results[0]->getName(), $results[0]->getFranchise()->getName());
                     }
-                    return sprintf("%s is owned by the %s.", $results[0]->getName(), $results[0]->getFranchise()->getName());
+                    return sprintf('%s is owned by the %s.', $results[0]->getName(), $results[0]->getFranchise()->getName());
                 }
-                return sprintf("%s is a free agent.", $results[0]->getName());
+                return sprintf('%s is a free agent.', $results[0]->getName());
             }
             // There is more than one match (this is unlikely)
             return "There's more than one player that matches that name. I'm not clever enough to continue.";
