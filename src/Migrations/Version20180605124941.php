7ce5f8820d81b8bb2c0b6b27739bc7a14b5950c4
Dan Abrey <danabrey@gmail.com>
2018-06-05 13:57:23 +0100

    Not flyer, fireflies, moron

--- src/Migrations/Version20180605124941.php
+++ src/Migrations/Version20180605124941.php
@@ -13,7 +13,7 @@ class Version20180605124941 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE franchise SET name = 'Flitwick Flyers', identifiers = 'flitwick,flyers' WHERE name = 'Bradford Championz'");
+        $this->addSql("UPDATE franchise SET name = 'Flitwick Fireflies', identifiers = 'flitwick,fireflies' WHERE name = 'Bradford Championz'");
         $this->addSql("UPDATE owner SET name = 'Mike', group_me_user_id = '60310674' WHERE name = 'Hamza'");
     }
 
