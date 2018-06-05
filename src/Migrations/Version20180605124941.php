d1085c66d358b5980ceb88dbcbdbfa276f43ea24
Dan Abrey <danabrey@gmail.com>
2018-06-05 13:53:30 +0100

    Update Hamza to Mike

---
+++ src/Migrations/Version20180605124941.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180605124941 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("UPDATE franchise SET name = 'Flitwick Flyers', identifiers = 'flitwick,flyers' WHERE name = 'Bradford Championz'");
+        $this->addSql("UPDATE owner SET name = 'Mike', group_me_user_id = '60310674' WHERE name = 'Hamza'");
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
