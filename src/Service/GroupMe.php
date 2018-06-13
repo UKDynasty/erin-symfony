dd188048b4f8df4fe24cfee99c44e3b71f0bd46f
Dan Abrey <danabrey@gmail.com>
2018-06-13 13:41:24 +0100

    Add logging when sending a message fails

--- src/Service/GroupMe.php
+++ src/Service/GroupMe.php
@@ -4,17 +4,24 @@ namespace App\Service;
 use App\GroupMe\DirectMessage;
 use App\GroupMe\GroupMessage;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\RequestOptions;
+use Psr\Log\LoggerInterface;
 
 class GroupMe
 {
     private $token;
     private $groupMeBotId;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct($groupMeDirectMessageToken, $groupMeBotId)
+    public function __construct($groupMeDirectMessageToken, $groupMeBotId, LoggerInterface $logger)
     {
         $this->token = $groupMeDirectMessageToken;
         $this->groupMeBotId = $groupMeBotId;
+        $this->logger = $logger;
     }
 
     public function getGroupMembers()
@@ -37,15 +44,19 @@ class GroupMe
 
         $url = "https://api.groupme.com/v3/direct_messages?token=" . $this->token;
 
-        $res = $client->request('POST', $url, [
-            RequestOptions::JSON => [
-                "direct_message" => [
-                    "source_guid" => uniqid(),
-                    "recipient_id" => $directMessage->getRecipientId(),
-                    "text" => $directMessage->getText(),
+        try {
+            $client->request('POST', $url, [
+                RequestOptions::JSON => [
+                    "direct_message" => [
+                        "source_guid" => uniqid(),
+                        "recipient_id" => $directMessage->getRecipientId(),
+                        "text" => $directMessage->getText(),
+                    ]
                 ]
-            ]
-        ]);
+            ]);
+        } catch (BadResponseException $exception) {
+            $this->logger->error('Bad response from GroupMe when sending direct message', ['directMessage' => json_encode($directMessage), 'response' => $exception->getResponse()->getBody()->getContents()]);
+        }
     }
 
     public function sendGroupMessage(GroupMessage $groupMessage)
@@ -54,12 +65,16 @@ class GroupMe
 
         $url = "https://api.groupme.com/v3/bots/post";
 
-        $res = $client->request('POST', $url, [
-            RequestOptions::JSON => [
-                "source_guid" => uniqid(),
-                "bot_id" => $this->groupMeBotId,
-                "text" => $groupMessage->getText(),
-            ]
-        ]);
+        try {
+            $client->request('POST', $url, [
+                RequestOptions::JSON => [
+                    "source_guid" => uniqid(),
+                    "bot_id" => $this->groupMeBotId,
+                    "text" => $groupMessage->getText(),
+                ]
+            ]);
+        } catch (BadResponseException $exception) {
+            $this->logger->error('Bad response from GroupMe when sending group message', ['groupMessage' => json_encode($groupMessage), 'response' => $exception->getResponse()->getBody()->getContents()]);
+        }
     }
 }
