983f3d703b4393753fc69e7dc721da886b8c42b3
Dan Abrey <danabrey@gmail.com>
2019-08-26 17:20:20 +0100

    Respond to cancel, help and stop requests

---
+++ src/Alexa/CancelRequestHandler.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Alexa;
+
+use App\Service\Erin;
+use MaxBeckers\AmazonAlexa\Helper\ResponseHelper;
+use MaxBeckers\AmazonAlexa\Helper\SsmlGenerator;
+use MaxBeckers\AmazonAlexa\Request\Request;
+use MaxBeckers\AmazonAlexa\Request\Request\Standard\IntentRequest;
+use MaxBeckers\AmazonAlexa\RequestHandler\AbstractRequestHandler;
+use MaxBeckers\AmazonAlexa\Response\Response;
+
+class CancelRequestHandler extends AbstractRequestHandler
+{
+    /**
+     * @var ResponseHelper
+     */
+    private $responseHelper;
+    /**
+     * @var Erin
+     */
+    private $erin;
+
+    public function __construct(ResponseHelper $responseHelper)
+    {
+
+        $this->responseHelper = $responseHelper;
+
+        $this->supportedApplicationIds = ['amzn1.ask.skill.5111ef87-31dc-461f-8455-23ca184dd885'];
+    }
+
+    public function supportsRequest(Request $request): bool
+    {
+        return $request->request instanceOf IntentRequest &&
+            'AMAZON.CancelIntent' === $request->request->intent->name;
+    }
+
+    public function handleRequest(Request $request): Response
+    {
+        return $this->responseHelper->respond('Talk soon!', true);
+    }
+}
